fd982eb9fab38728cdf92bd054806798359ac446
Andressa Santos <79110143+andressa-santtos@users.noreply.github.com>
2023-06-13 19:13:37 -0300

    teste do teste2

--- src/pages/CadastrarColetor/coletor.tsx
+++ src/pages/CadastrarColetor/coletor.tsx
@@ -151,7 +151,6 @@ export default function CadastrarColetor() {
                     resizeMode='contain'
                 />
             </View>
-            <
 
             <animatable.View animation="fadeInUp" style={styles.containerForm}>
                 <ScrollView style={styles.form}>
